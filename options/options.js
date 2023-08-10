8c7a6684ee4f80cc4f7355a83a5c3ebca34e0dc5
Crowdbotics <team@crowdbotics.com>
2023-08-10 21:51:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-bread-42781",
+  url: "https://weathered-bread-42781.botics.co",
+  api: "https://weathered-bread-42781.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
